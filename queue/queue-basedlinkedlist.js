b9637d8ef0eabe999f8a79065604ba8a561e3288
zhonghui.liao <zhonghui.liao@zhongheschool.com>
2020-12-07 14:08:28 +0800

    增加队列实现：链表实现队列

---
+++ queue/queue-basedlinkedlist.js
@@ -0,0 +1,52 @@
+/**
+ * 链式队列
+ * api: enqueue, dequeue
+ */
+
+class Node {
+    constructor(element) {
+        this.element = element;
+        this.next = null;
+    }
+}
+
+class QueueBasedLinkedList {
+    constructor () {
+        this.head = null;
+        this.tail = null;
+    }
+    // 入队
+    enqueue (element) {
+        if (!this.head) {
+            this.head = new Node(element);
+            this.tail = this.head;
+        } else {
+            this.tail.next = new Node(element);
+            this.tail = this.tail.next;
+        }
+    }
+
+    // 出队
+    dequeue () {
+        if (!this.head) return -1;
+        else {
+            let value = this.head.element;
+            this.head = this.head.next;
+            return value;
+        }
+    }
+}
+
+/**
+ * test cases
+ */
+const queue = new QueueBasedLinkedList();
+queue.enqueue(1);
+queue.enqueue(2);
+queue.enqueue(3);
+
+let res = 0;
+while(res != -1) {
+    res = queue.dequeue();
+    console.log(res);
+}
